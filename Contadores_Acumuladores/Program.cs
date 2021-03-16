6b50cf56209540cc782286f0e72406d8ee1d3e8a
Carlos Rodriguez <00402720@uca.edu.sv>
2021-03-15 20:55:19 -0400

    Program finalized

--- Contadores_Acumuladores/Program.cs
+++ Contadores_Acumuladores/Program.cs
@@ -6,7 +6,20 @@ namespace Contadores_Acumuladores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = 0;
+            Console.Write("Given n = ");
+            n = Int32.Parse(Console.ReadLine());
+            int reduce = n;
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < reduce; j++)
+                {
+                    Console.Write($"{n - j} ");
+                }
+                Console.WriteLine("");
+                reduce--;
+            }
         }
     }
 }
